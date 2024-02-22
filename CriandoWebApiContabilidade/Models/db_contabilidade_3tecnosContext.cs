c078de3007a48c31830e592f9229476620fc2cd1
Andressa <115846058+andressa-l@users.noreply.github.com>
2024-02-21 22:59:35 -0300

    Update db_contabilidade_3tecnosContext.cs

--- CriandoWebApiContabilidade/Models/db_contabilidade_3tecnosContext.cs
+++ CriandoWebApiContabilidade/Models/db_contabilidade_3tecnosContext.cs
@@ -31,8 +31,7 @@ namespace CriandoWebApiContabilidade.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data source=PC03LAB2521\\SENAI; Database=db_contabilidade_3tecnos; User Id=sa; Password=senai.123;");
+
             }
         }
 
